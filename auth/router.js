7e918c31dc7f892fc6a069170a54afcb46d92707
janecyyu <janecyyu@gmail.com>
2020-05-18 14:42:36 -0700

    login ok

--- auth/router.js
+++ auth/router.js
@@ -26,4 +26,34 @@ router.post("/register", (req, res) => {
   }
 });
 
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (isValid(req.body)) {
+    Users.findBy({ username })
+      //.first()
+      .then(([user]) => {
+        console.log(user);
+        // check that passwords match
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          //save info about client inside the session(req.session)
+        //   req.session.loggedIn = true;
+        //   req.session.user = user;
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          // we will return 401 if the password or username are invalid
+          // we don't want to let attackers know when they have a good username
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "please provide username and password and the password should be alphanumeric",
+    });
+  }
+});
+
 module.exports = router;
--- users/userModel.js
+++ users/userModel.js
@@ -20,6 +20,7 @@ async function add(user) {
 
   return findById(id);
 }
+
 function findById(id) {
     return db("users")
       .where({ id })
